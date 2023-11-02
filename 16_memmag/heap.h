01fb4ffd7418f7304c97cb463fd5189a709a3df5
GreyZhang <GreyZhang@126.com>
2023-11-02 15:48:33 +0800

    heap_4.c from FreeRTOS.

---
+++ 16_memmag/heap.h
@@ -0,0 +1,59 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/* Definition assert() function. */
+#define configASSERT(x)                  if((x)==0) { for( ;; ); }
+#define configSUPPORT_DYNAMIC_ALLOCATION 1
+#define configAPPLICATION_ALLOCATED_HEAP 0
+#define configTOTAL_HEAP_SIZE            8192
+#define PRIVILEGED_FUNCTION
+#define portBYTE_ALIGNMENT               8
+#define PRIVILEGED_DATA
+#define portMAX_DELAY                    ( uint32_t ) 0xffffffffUL
+
+#if portBYTE_ALIGNMENT == 32
+    #define portBYTE_ALIGNMENT_MASK    ( 0x001f )
+#elif portBYTE_ALIGNMENT == 16
+    #define portBYTE_ALIGNMENT_MASK    ( 0x000f )
+#elif portBYTE_ALIGNMENT == 8
+    #define portBYTE_ALIGNMENT_MASK    ( 0x0007 )
+#elif portBYTE_ALIGNMENT == 4
+    #define portBYTE_ALIGNMENT_MASK    ( 0x0003 )
+#elif portBYTE_ALIGNMENT == 2
+    #define portBYTE_ALIGNMENT_MASK    ( 0x0001 )
+#elif portBYTE_ALIGNMENT == 1
+    #define portBYTE_ALIGNMENT_MASK    ( 0x0000 )
+#else /* if portBYTE_ALIGNMENT == 32 */
+    #error "Invalid portBYTE_ALIGNMENT definition"
+#endif /* if portBYTE_ALIGNMENT == 32 */
+
+/* Used to pass information about the heap out of vPortGetHeapStats(). */
+typedef struct xHeapStats
+{
+    /* The total heap size currently available - this is the sum of all the free blocks, not the largest block that can be allocated. */
+    size_t xAvailableHeapSpaceInBytes;
+    /* The maximum size, in bytes, of all the free blocks within the heap at the time vPortGetHeapStats() is called. */
+    size_t xSizeOfLargestFreeBlockInBytes;
+    /* The minimum size, in bytes, of all the free blocks within the heap at the time vPortGetHeapStats() is called. */
+    size_t xSizeOfSmallestFreeBlockInBytes;
+    /* The number of free memory blocks within the heap at the time vPortGetHeapStats() is called. */
+    size_t xNumberOfFreeBlocks;
+    /* The minimum amount of total free memory (sum of all free blocks) there has been in the heap since the system booted. */
+    size_t xMinimumEverFreeBytesRemaining;
+    /* The number of calls to pvPortMalloc() that have returned a valid memory block. */
+    size_t xNumberOfSuccessfulAllocations;
+    /* The number of calls to vPortFree() that has successfully freed a block of memory. */
+    size_t xNumberOfSuccessfulFrees;
+} HeapStats_t;
+
+
+size_t xPortGetFreeHeapSize(void);
+void vPortGetHeapStats(HeapStats_t * pxHeapStats);
+size_t xPortGetMinimumEverFreeHeapSize( void );
+void vPortFree( void * pv );
+void * pvPortMalloc( size_t xWantedSize );
+
+#endif
---
+++ 16_memmag/test.c
@@ -0,0 +1,68 @@
+#include "heap.h"
+#include <stdio.h>
+
+void report_heap_status(void)
+{
+    HeapStats_t status;
+
+    vPortGetHeapStats(&status);
+
+    printf("************ status of heap ************\n");
+    printf("xAvailableHeapSpaceInBytes = %ld\n", status.xAvailableHeapSpaceInBytes);
+    printf("xMinimumEverFreeBytesRemaining = %ld\n", status.xMinimumEverFreeBytesRemaining);
+    printf("xNumberOfFreeBlocks = %ld\n", status.xNumberOfFreeBlocks);
+    printf("xNumberOfSuccessfulAllocations = %ld\n", status.xNumberOfSuccessfulAllocations);
+    printf("xNumberOfSuccessfulFrees = %ld\n", status.xNumberOfSuccessfulFrees);
+    printf("xSizeOfLargestFreeBlockInBytes = %ld\n", status.xSizeOfLargestFreeBlockInBytes);
+    printf("xSizeOfSmallestFreeBlockInBytes = %ld\n", status.xSizeOfSmallestFreeBlockInBytes);
+}
+
+int main(void)
+{
+    /* printf("start initialisation...\n"); */
+    /* memmang_init(); */
+    /* int free_size_init = xPortGetFreeHeapSize(); */
+    /* printf("xPortGetFreeHeapSize() = %d\n", free_size_init); */
+    /* printf("prvHeapInit ram cost: %d\n", configTOTAL_HEAP_SIZE - free_size_init); */
+    /* report_heap_status(); */
+
+    printf("-----------------------------------------------------------------------------\n");
+    printf("sizeof int is :%ld\n", sizeof(int));
+    int free_size_before = xPortGetFreeHeapSize();
+    printf("free memory size before malloc a int size: %d\n", free_size_before);
+    int *a = pvPortMalloc(sizeof(int));
+    int free_size_after = xPortGetFreeHeapSize();
+    printf("free memory size after malloc a int size = %d\n", free_size_after);
+    printf("ram cost for a int malloc: %d\n", free_size_before - free_size_after);
+    report_heap_status();
+
+
+    printf("-----------------------------------------------------------------------------\n");
+    printf("sizeof 2 int is :%ld\n", 2 * sizeof(int));
+    free_size_before = xPortGetFreeHeapSize();
+    printf("free memory size before malloc 2 int size: %d\n", free_size_before);
+    int *b = pvPortMalloc(2 * sizeof(int));
+    free_size_after = xPortGetFreeHeapSize();
+    printf("free memory size after malloc 2 int size = %d\n", free_size_after);
+    printf("ram cost for 2 int malloc: %d\n", free_size_before - free_size_after);
+    report_heap_status();
+
+    printf("-----------------------------------------------------------------------------\n");
+    printf("sizeof 8 int is :%ld\n", 8 * sizeof(int));
+    free_size_before = xPortGetFreeHeapSize();
+    printf("free memory size before malloc 8 int size: %d\n", free_size_before);
+    int *c = pvPortMalloc(8 * sizeof(int));
+    free_size_after = xPortGetFreeHeapSize();
+    printf("free memory size after malloc 8 int size = %d\n", free_size_after);
+    printf("ram cost for 8 int malloc: %d\n", free_size_before - free_size_after);
+    report_heap_status();
+    vPortFree(a);
+    report_heap_status();
+    vPortFree(b);
+    report_heap_status();
+    vPortFree(c);
+    report_heap_status();
+    printf("heap size after free all blocks: %d\n", xPortGetFreeHeapSize());
+
+    return 0;
+}
